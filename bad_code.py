215fcf52405d6e2dd70ccc822a7dc7d6119d2ba0
Teesha Yadav <tishayadav2004@gmail.com>
2026-08-08 20:38:02 +0530

    Add initial test code

---
+++ bad_code.py
@@ -0,0 +1,22 @@
+def calculate_average(numbers):
+    total = 0
+
+    for number in numbers:
+        total += number
+
+    return total / len(numbers)
+
+
+def get_user(users, user_id):
+    for user in users:
+        if user["id"] == user_id:
+            return user
+
+    return None
+
+
+def login(username, password):
+    if username == "admin" and password == "admin123":
+        return True
+
+    return False
